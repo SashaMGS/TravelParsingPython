72bf45c501bb1f96c332561133bbb51303a77267
SashaMGS <166273701+SashaMGS@users.noreply.github.com>
2024-04-07 14:26:39 +1000

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import csv
-with open('bitcoin.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(''.join(row))
